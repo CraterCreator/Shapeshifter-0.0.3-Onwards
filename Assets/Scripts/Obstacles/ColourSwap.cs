7b59e5ab3ad962468d5de85b3839998b76102ce4
CraterCreator <jordanfischer13@gmail.com>
2018-11-15 19:25:33 +1100

    Version 1.5.0
    Created Obstacle art and implemented with colour meshing with the background

---
+++ Assets/Scripts/Obstacles/ColourSwap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourSwap : MonoBehaviour
+{
+    private SpriteRenderer rend, colorRend;
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        colorRend = GameObject.Find("Right Neon").GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.color = colorRend.color;
+    }
+}
